721772a730484887100c51984287700e3e816302
arun <arun1nly1@gmail.com>
2024-11-27 16:40:40 +0545

    variable and data-types

---
+++ 2.variables.js
@@ -0,0 +1,79 @@
+//? variable => rewritable storage
+
+//? syntax
+// let  variableName = value
+
+// let userName = 'Saurav Shrestha';
+// console.log(userName);
+// userName = 'Ashesh Mishra';
+// console.log(userName);
+
+// let x = 2;
+// console.log(x);
+// x = 10;
+// console.log(x);
+
+// ? variable naming convention
+
+// 1. variable name must not contain reserved keywords
+// e.g. for, while, switch, let, const, break, do, function etc...
+
+// 2. variable name must not start with numbers
+
+// !let 2number = 5; // wrong
+// let number2 = 10; // right
+
+// 3. variable name must not contain special characters
+//  such as  @ ,#, %, +, - etc. except $ and _
+
+// !let name# =2  //wrong
+
+// let user_name = 'John'; //right
+
+// let price$ = 25; //right
+
+// let _weight = 60; //right
+
+// let _ = 5; //right
+
+// let $ = 'Nepal'; //right
+
+// 4. variable name should be descriptive
+// t means temperature
+// let t = 25;
+
+// let temperature = 25; // right approach
+// console.log(25);
+
+// let country = 'Nepal';
+
+// 5. variable name should be camelCase
+
+// let user_name = 'John'; //snake case
+// let userName = 'John'; // right approach
+// let username =""
+
+// let firstName = 'Rina';
+// let lastName = 'Maharjan';
+// let phoneNumber = '9766123456';
+// let highestMarks = 99;
+
+// let max_student_height = 5.6; //snake case // wrong approach
+// let maxStudentHeight = 5.6; //right approach
+
+// ?constant =>read only storage
+
+// ? syntax
+// const constantName =value
+
+// const PI = 3.1415;
+
+// console.log(PI);
+
+// ? universal constant are capital case
+
+// const GRAVITY_ON_EARTH = 9.8;
+
+// const x = 9;
+
+// const userName = 'Rohan';
---
+++ 3.data.types.js
@@ -0,0 +1,11 @@
+// data types
+// ? type of data which memory can hold
+
+// ? 1. string
+// ? 2. number
+// ? 3. boolean
+// ? 4. undefined
+// ? 5. null
+// ? 6. object
+// ? 7. bigInt
+// ? 8. symbol
